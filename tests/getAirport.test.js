1cd7fdd60e4797281a1ccd41e4387a1cc3ed7065
Rahul Singh <rahul.singh12221998@gmail.com>
2021-09-03 21:29:01 +0530

    adds tests for US

--- tests/getAirport.test.js
+++ tests/getAirport.test.js
@@ -1,30 +1,14 @@
 const fs = require('fs');
 const getAirport = require('../app/getAirport');
 
-describe('India', () => {
-  test('valid', async () => {
-    const icao = "VABB";
-    const airport = getAirport(icao);
-    const expecting = {
-      id: '26434',
-      ident: 'VABB',
-      type: 'large_airport',
-      name: 'Chhatrapati Shivaji International Airport',
-      latitude_deg: '19.0886993408',
-      longitude_deg: '72.8678970337',
-      elevation_ft: '39',
-      continent: 'AS',
-      iso_country: 'IN',
-      iso_region: 'IN-MM',
-      municipality: 'Mumbai',
-      scheduled_service: 'yes',
-      gps_code: 'VABB',
-      iata_code: 'BOM',
-      local_code: '',
-      home_link: 'http://www.csia.in/',
-      wikipedia_link: 'https://en.wikipedia.org/wiki/Chhatrapati_Shivaji_International_Airport',
-      keywords: 'Bombay, Sahar International Airport'
-    }
-    expect(airport).toMatchObject(expecting);
-  });
+test('IN', async () => {
+  const icao = 'VABB';
+  const airport = getAirport(icao);
+  expect(airport).toMatchObject({ ident: 'VABB' });
+});
+
+test('US', async () => {
+  const icao = 'KJFK';
+  const airport = getAirport(icao);
+  expect(airport).toMatchObject({ ident: 'KJFK' });
 });
--- tests/getChart.test.js
+++ tests/getChart.test.js
@@ -5,8 +5,10 @@ const getChart = require('../app/getChart');
 const playbooks_dir = `${process.cwd()}/playbooks`;
 const uriRegEx = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
 
-describe('India', () => {
-  test('valid', async () => {
+jest.setTimeout(30000);
+
+describe('IN', () => {
+  test('Found', async () => {
     const icao = "VABB";
     const airport = getAirport(icao);
     const _playbook = fs.readFileSync(`${playbooks_dir}/${airport.iso_country}.json`, 'utf8');
@@ -15,8 +17,28 @@ describe('India', () => {
     expect(chart).toMatch(uriRegEx);
   });
 
-  test('invalid', async () => {
+  test('Not Found', async () => {
     const icao = "IN-0030";
+    const airport = getAirport(icao);
+    const _playbook = fs.readFileSync(`${playbooks_dir}/${airport.iso_country}.json`, 'utf8');
+    const playbook = JSON.parse(_playbook);
+    const chart = await getChart(playbook, icao);
+    expect(chart).toMatch("error");
+  });
+});
+
+describe('US', () => {
+  test('Found', async () => {
+    const icao = "KJFK";
+    const airport = getAirport(icao);
+    const _playbook = fs.readFileSync(`${playbooks_dir}/${airport.iso_country}.json`, 'utf8');
+    const playbook = JSON.parse(_playbook);
+    const chart = await getChart(playbook, icao);
+    expect(chart).toMatch(uriRegEx);
+  });
+
+  test('Not Found', async () => {
+    const icao = "00AZ";
     const airport = getAirport(icao);
     const _playbook = fs.readFileSync(`${playbooks_dir}/${airport.iso_country}.json`, 'utf8');
     const playbook = JSON.parse(_playbook);

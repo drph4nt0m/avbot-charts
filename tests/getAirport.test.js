35d982c90e9e6075bb92b234de5ccc137523c546
Nicolás Marulanda P <nicomaru@outlook.com>
2021-10-06 16:57:27 +0200

    fix: prettier check is failing - run "npm run prettier" and push changes again.

--- tests/getAirport.test.js
+++ tests/getAirport.test.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const getAirport = require('../app/getAirport');
 const airports = require('./airports.json');
 
-describe.each(airports)('$iso', ({ident}) => {
+describe.each(airports)('$iso', ({ ident }) => {
   test(ident, async () => {
     const airport = getAirport(ident);
     expect(airport).toMatchObject({ ident });

d8b38f2b0bc6cfe57db19f5cfceec7aca7e50912
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2021-10-07 00:14:59 +0200

    311 use chartIdent in getAirport tests

--- tests/getAirport.test.js
+++ tests/getAirport.test.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 const getAirport = require('../app/getAirport');
 const airports = require('./airports.json');
 
-describe.each(airports)('$iso', ({ ident }) => {
-  test(ident, async () => {
-    const airport = getAirport(ident);
-    expect(airport).toMatchObject({ ident });
+describe.each(airports)('$iso', ({ chartIdent }) => {
+  test(chartIdent.valid, async () => {
+    const airport = getAirport(chartIdent.valid);
+    expect(airport).toMatchObject({ ident: chartIdent.valid });
   });
 });
